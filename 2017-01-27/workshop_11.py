29256774562980aa56c92cf2c10718dbe4cd497f
carloLV <carlo.laviola@gmail.com>
2017-01-28 17:06:58 +0100

    improvement workshop 11

--- 2017-01-27/workshop_11.py
+++ 2017-01-27/workshop_11.py
@@ -2,20 +2,36 @@ from pyplasm import *
 import csv
 from help import workshop_10 as w10
 from help import workshop_05 as w5
+from help import workshop_7 as w7
+
+x1=[.05,1,.05,1,.05,1,.05]
+y1=[.05,.7,.1,.7,.05]
+b1=[[1,1,1,1,1,1,1],[1,0,1,0,1,0,1],[1,1,1,1,1,1,1],[1,0,1,0,1,0,1],[1,1,1,1,1,1,1]]
+	
 
 def village_builder():
 
 	c=MAP(BEZIERCURVE([[-0,0],[1,0],[1,1],[2,1],[3,1]]))(INTERVALS(1)(32))
 	return c
 
-
-def create_school():
-	building = CUBOID([11,9,4])
-	diff = CUBOID([10,8,3])
+"""
+def create_school(xMax,yMax):
+	
+	building = CUBOID([xMax,yMax,4])
+	diff = CUBOID([xMax-1,yMax-1,3])
 	classRoom = DIFFERENCE([building,T([1,2,3])([.5,.5,.5])(diff)])
-	furniture = w5.ggpl_create_furniture(10,8,3)
+	furniture = w5.ggpl_create_furniture(xMax-1,yMax-2,3)
 	classRoom = STRUCT([classRoom,T([1,2,3])([.5,.5,.5])(furniture)])
-	return classRoom
+	
+	window = w7.ggpl_window(x1,y1,b1)(.8,.8,.1)
+	VIEW(window)
+	window = T(3)(2)(window)
+
+	classRoom = DIFFERENCE([classRoom,window])
+	classRoom = STRUCT([classRoom,window])
+	
+	VIEW(classRoom)
+	return classRoom"""
 
 def create_bench(r,h):
 	#bench's base dimensions
@@ -92,7 +108,7 @@ def build_fountain(r,h):
 		
 	benches = STRUCT([bench1,bench2,bench3,bench4])
 	complete = STRUCT([fountain,treeSet,benches])
-	VIEW(complete)
+	
 	return complete
 	
 	
@@ -109,6 +125,16 @@ def build_nbood():
 	quartier = STRUCT([houses,houses2])
 	quartier = S([1,2,3])([.8,.8,.8])(quartier)
 	return quartier
+
+def build_bifamiliar_house():
+	house = w10.house_builder('help/lines_file/')
+	tree = tree_creator(SIZE(3)(house))
+	house = STRUCT([house,tree,T(2)(SIZE(2)(house))(tree),T(1)(SIZE(1)(house))(tree)])
+	house = T(1)(SIZE(1)(house)/2)(house)
+	house2 = T(1)(-SIZE(1)(house))(house)
+	houses = STRUCT([house,house2])
+	tree = tree_creator(SIZE(3)(house))
+	return houses
 	
 
 def build_roads(pts):
@@ -136,7 +162,7 @@ def build_roads(pts):
 
 if __name__ == '__main__':
 	
-	points = [[[60,0],[58,7.5],[60,15],[52,18],[46,21],[42,28],[40,32],[40,60],[40,80]],
+	points = [[[60,0],[58,7.5],[60,15],[52,18],[46,21],[42,28],[40,32],[40,60],[40,80],[40,90]],
 	[[42,60],[46,60],[52,60],[60,60],[65,60],[74,63],[82,66],[90,69],[100,72],[110,75]],
 	[[60,15],[65,15],[74,18],[82,20],[90,22],[95,25],[100,30],[110,38],[115,42]],
 	[[60,15],[60,21],[60,38],[60,41],[60,60],[60,63]],
@@ -157,12 +183,19 @@ if __name__ == '__main__':
 	fact2 = SIZE(2)(q1)+SIZE(2)(q1)/3
 	q2 = T(2)(fact2)(q1)
 
-	#school = create_school()
-	#school = T(1)(x)(school)
+	q3 = build_bifamiliar_house()
+	q3 = T([1,2])([x/2,y-SIZE(2)(q3)])(q3)
+
+
+	tree = tree_creator(SIZE(3)(q1))
+	treeDistance = SIZE(1)(tree) + SIZE(1)(tree)/2
+	wood = tree
+	wood = STRUCT([wood,T(1)(treeDistance)(tree),T(2)(treeDistance)(tree),T(1)(-treeDistance)(tree),T(2)(-treeDistance)(tree)]) 
+	wood = T([1,2])([fact1*2,fact2])(wood)
 	
 	fountain = build_fountain(SIZE(1)(q1)/6,SIZE(3)(q1))
 	fountain = T([1,2])([fact1*3.5,fact2])(fountain)
-	st = STRUCT([le,table,q1,q2,fountain])
+	st = STRUCT([le,table,q1,q2,q3,fountain,wood])
 	
 	VIEW(st)
 

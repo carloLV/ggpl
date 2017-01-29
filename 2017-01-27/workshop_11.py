b5aacef09cf98d2162b129eb5e5a31e7313016c2
carloLV <carlo.laviola@gmail.com>
2017-01-29 12:47:41 +0100

    code complete; working on .ipynb

--- 2017-01-27/workshop_11.py
+++ 2017-01-27/workshop_11.py
@@ -8,30 +8,20 @@ x1=[.05,1,.05,1,.05,1,.05]
 y1=[.05,.7,.1,.7,.05]
 b1=[[1,1,1,1,1,1,1],[1,0,1,0,1,0,1],[1,1,1,1,1,1,1],[1,0,1,0,1,0,1],[1,1,1,1,1,1,1]]
 	
-
+"""
 def village_builder():
 
 	c=MAP(BEZIERCURVE([[-0,0],[1,0],[1,1],[2,1],[3,1]]))(INTERVALS(1)(32))
 	return c
 
 """
-def create_school(xMax,yMax):
-	
-	building = CUBOID([xMax,yMax,4])
-	diff = CUBOID([xMax-1,yMax-1,3])
-	classRoom = DIFFERENCE([building,T([1,2,3])([.5,.5,.5])(diff)])
-	furniture = w5.ggpl_create_furniture(xMax-1,yMax-2,3)
-	classRoom = STRUCT([classRoom,T([1,2,3])([.5,.5,.5])(furniture)])
-	
-	window = w7.ggpl_window(x1,y1,b1)(.8,.8,.1)
-	VIEW(window)
-	window = T(3)(2)(window)
-
-	classRoom = DIFFERENCE([classRoom,window])
-	classRoom = STRUCT([classRoom,window])
-	
-	VIEW(classRoom)
-	return classRoom"""
+def create_sign(h):
+	pillar = CYLINDER([.3,h])(20)
+	pillar = MATERIAL([.13,.13,.13,1,  0,0,0,1,  0,0,0,0, 0,0,0,0, 1])(pillar)
+	sign = CUBOID([h,.3,h/2])
+	sign = TEXTURE("texture/logo.jpg")(sign)
+	sign = STRUCT([pillar,T([1,3])([-h/2,h])(sign)])
+	return sign
 
 def create_bench(r,h):
 	#bench's base dimensions
@@ -111,6 +101,19 @@ def build_fountain(r,h):
 	
 	return complete
 	
+def child_game(r):
+	base = CYLINDER([r,1])(16)
+	base = MATERIAL([.5,.5,.5,1,  0,0,0,1,  0,0,0,0, 0,0,0,0, 1])(base)	
+	fulcro = CYLINDER([r/18,4])(16)
+	fulcro = MATERIAL([.13,.13,.13,1,  0,0,0,1,  0,0,0,0, 0,0,0,0, 1])(fulcro)
+	handle = CYLINDER([r,3])(16)	
+	handle = SKEL_1(handle)
+	handle = OFFSET([.3,.3,.3])(handle)
+	handle =  MATERIAL([.13,.13,.13,1,  0,0,0,1,  0,0,0,0, 0,0,0,0, 1])(handle)
+	controller = CYLINDER([r/5,.5])(16)
+	controller = MATERIAL([1,.2,.4,1,  0,0,0,1,  0,0,0,0, 0,0,0,0, 1])(controller)
+	s = STRUCT([base,fulcro,T(3)(4)(controller),handle])
+	return s
 	
 
 
@@ -133,8 +136,14 @@ def build_bifamiliar_house():
 	house = T(1)(SIZE(1)(house)/2)(house)
 	house2 = T(1)(-SIZE(1)(house))(house)
 	houses = STRUCT([house,house2])
-	tree = tree_creator(SIZE(3)(house))
+	houses = S([1,2,3])([.8,.8,.8])(houses)
 	return houses
+
+def build_single_house():
+	house = w10.house_builder('help/lines_file/')
+	tree = tree_creator(SIZE(3)(house))
+	house = STRUCT([house,tree,T(2)(SIZE(2)(house))(tree),T(1)(SIZE(1)(house))(tree)])
+	return house
 	
 
 def build_roads(pts):
@@ -173,29 +182,38 @@ if __name__ == '__main__':
 	x,y=SIZE([1,2])(le)
 	
 	table = CUBOID([x,y,5])
-	table = MATERIAL([.5,.5,.5,1,  0,0,0,1,  0,0,0,0, 0,0,0,1, 1])(table)
+	table = MATERIAL([.6,.3,0,1,  0,0,0,1,  0,0,0,0, 0,0,0,1, 1])(table)
 	table = T(3)(-5.2)(table)
 
 	q1=build_nbood()
 	fact1 = SIZE(2)(q1)- SIZE(2)(q1)/3
-	q1=T(2)(fact1)(q1)	
+	q1=T(2)(fact1)(q1)		
 
 	fact2 = SIZE(2)(q1)+SIZE(2)(q1)/3
 	q2 = T(2)(fact2)(q1)
 
 	q3 = build_bifamiliar_house()
-	q3 = T([1,2])([x/2,y-SIZE(2)(q3)])(q3)
+	q3 = R([1,2])(PI)(q3)	
+	q3 = T([1,2])([SIZE(1)(q1)*2+3,y-1])(q3)
 
+	q4 = build_single_house()
+	q4 = T(1)(x-SIZE(1)(q4))(q4)
+
+	game = child_game(SIZE(1)(q4)/4)
+	game = T([1,2])([x/2+SIZE(1)(game)*1.5+3,SIZE(2)(game)-2])(game)
 
 	tree = tree_creator(SIZE(3)(q1))
 	treeDistance = SIZE(1)(tree) + SIZE(1)(tree)/2
 	wood = tree
 	wood = STRUCT([wood,T(1)(treeDistance)(tree),T(2)(treeDistance)(tree),T(1)(-treeDistance)(tree),T(2)(-treeDistance)(tree)]) 
-	wood = T([1,2])([fact1*2,fact2])(wood)
-	
-	fountain = build_fountain(SIZE(1)(q1)/6,SIZE(3)(q1))
+	wood = T([1,2])([fact1*2-2,SIZE(2)(wood)/2])(wood)
+
+	sign = create_sign(SIZE(3)(q1))
+	sign = T([1,2])([fact1*2,fact2])(sign)
+
+	fountain = build_fountain(SIZE(1)(q1)/5,SIZE(3)(q1))
 	fountain = T([1,2])([fact1*3.5,fact2])(fountain)
-	st = STRUCT([le,table,q1,q2,q3,fountain,wood])
+	st = STRUCT([le,table,q1,q2,q3,q4,fountain,wood,sign,game])
 	
 	VIEW(st)
 
